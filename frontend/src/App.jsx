f5fe22a9ce398562b414823c5158435ffc7fe540
Peter Gniewek <petergmail@hotmail.com>
2026-02-18 21:37:43 -0500

    Enable React Router v7 startTransition future flag

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true }}>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route
